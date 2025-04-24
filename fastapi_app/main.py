1655f745013e797e5fb62025fbb86a581170fda0
cryptorexx <takealiftoff@gmail.com>
2025-04-24 17:44:37 +0500

    Update main.py

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -3,9 +3,18 @@ import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://aampav-frontend.onrender.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Define your routes here
-
+@app.get("/status")
+def get_status():
+    return {"message": "Backend is connected and running successfully."}
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
