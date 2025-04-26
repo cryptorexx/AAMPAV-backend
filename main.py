a2afe114de3c8a015b35b1277c343a5d37ed3db2
cryptorexx <takealiftoff@gmail.com>
2025-04-26 15:50:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Enable frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://aampav-frontend.onrender.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
