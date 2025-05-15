4e419231d1ab7a194d865e238fa0f616ff00cffc
cryptorexx <takealiftoff@gmail.com>
2025-05-15 19:26:27 +0500

    Update auto_broker_handler.py

--- execution_ai/brokers/auto_broker_handler.py
+++ execution_ai/brokers/auto_broker_handler.py
@@ -3,7 +3,10 @@
 from execution_ai.brokers.alpaca_broker import AlpacaBroker
 from execution_ai.brokers.base_broker import BaseBroker
 from execution_ai.brokers.universal_broker import UniversalBroker
-from execution_ai.brokers.broker_utils import load_brokers, save_brokers
+from execution_ai.brokers import broker_interface
+
+brokers = broker_interface.load_brokers()
+broker_interface.save_brokers(brokers)
 
 class AutoBrokerHandler:
     def scan_and_select(self):
