835dae770667ce7ddfed6bfdedf4a336e298983a
cryptorexx <takealiftoff@gmail.com>
2025-05-17 14:45:20 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 uvicorn main:app --host 0.0.0.0 --port 8000
 export API_KEY="-_k7HtLtIyxUuh2HMj5mSVSvpFUxzYYkmD8asOniC3U"
+
+#!/bin/bash
+python3 encryption_utils.py --init
+exec uvicorn main:app --host 0.0.0.0 --port 10000
